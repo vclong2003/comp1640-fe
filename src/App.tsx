7a674c2c00f6d146392d2a4f7c734feae8b59c24
vclong2003 <vclong2003@gmail.com>
2024-01-24 18:58:42 -0800

    install packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">test</div>;
 }
 
 export default App;

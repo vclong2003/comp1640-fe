42b2ef9b7ed1e94025a9f789df3359caf0a6b782
vclong2003 <vclong2003@gmail.com>
2024-03-14 20:20:42 +0700

    change docker to yarn

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,11 @@ WORKDIR /app
 
 COPY . .
 
-RUN npm install -g serve
-RUN npm install
-RUN npm run build
+# RUN npm install -g serve
+# RUN npm install
+# RUN npm run build:prod
+
+RUN yarn global add serve && yarn install && yarn build:prod
 
 EXPOSE 4200
 CMD ["serve", "-s", "-l", "4200", "./build"]
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AuthorizedPage from "../../components/Authorized/AuthorizedPage";
 import { ERole } from "../../enums/user.enums";
 
-const Home = (): JSX.Element => {
+const Home = () => {
   // add auth page for testing
   return (
     <AuthorizedPage allowedRoles={[ERole.Student, ERole.Admin]}>

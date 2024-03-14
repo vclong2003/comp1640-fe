9b315689740963e683f8b7cf1e688ce2a8e96c0e
vclong2003 <vclong2003@gmail.com>
2024-03-14 15:04:45 +0700

    ok

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,17 +5,22 @@ import DefaultLayout from "../components/Layouts/DefaultLayout";
 import ManagementLayout from "../components/Layouts/ManagementLyout";
 import AuthLayout from "../components/Layouts/AuthLayout";
 import { defaultLayoutRoutes } from "./defaultLayoutRoutes";
+import { authRoutes } from "./authRoutes";
 
 export default function AppRoutes(): JSX.Element {
   return (
     <Routes>
+      <Route path="/auth" element={<AuthLayout />}>
+        {authRoutes.map(({ path, component }) => (
+          <Route key={path} path={path} element={component} />
+        ))}
+      </Route>
       <Route path="/" element={<DefaultLayout />}>
         {defaultLayoutRoutes.map(({ path, component }) => (
           <Route key={path} path={path} element={component} />
         ))}
       </Route>
       <Route path="/management" element={<ManagementLayout />}></Route>
-      <Route path="/auth" element={<AuthLayout />}></Route>
     </Routes>
   );
 }
---
+++ src/routes/authRoutes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { IRoute } from "../interfaces";
+import Login from "../pages/authPages/Login/Login";
+import SetupAccount from "../pages/authPages/SetupAccount/SetupAccount";
+
+export const authRoutes: IRoute[] = [
+  {
+    path: "/login",
+    component: <Login />,
+  },
+  {
+    path: "/setup-account/:token",
+    component: <SetupAccount />,
+  },
+];

b93feab55f7846dd4fe7cb02cf92ba7ed29ed4ee
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-03-19 16:51:02 +0700

    Edit Profile Page

---
+++ src/pages/EditProfifle/MiddleForm/MiddleForm.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import * as S from "./MiddleForm.styled";
+
+const MiddleForm = () => {
+  return (
+    <S.Container>
+      <S.LeftContainer>
+        <S.Name>
+          <S.FirstName>
+            <S.Text>First Name</S.Text>
+            <input type="text" name="" id="" />
+          </S.FirstName>
+          <S.LastName>
+            <S.Text>Last Name</S.Text>
+            <input type="text" name="" id="" />
+          </S.LastName>
+        </S.Name>
+        <S.Email>
+          <S.Text>Email</S.Text>
+          <input type="email" />
+        </S.Email>
+        <S.Address>
+          <S.Text>Address</S.Text>
+          <input type="text" />
+        </S.Address>
+        <S.ContactNumber>
+          <S.Text>Contact Number</S.Text>
+          <input type="number" />
+        </S.ContactNumber>
+        <S.Password>
+          <S.Text>Password</S.Text>
+          <input type="password" />
+        </S.Password>
+      </S.LeftContainer>
+      <S.RightContainer>
+        <S.Avatar>
+          <S.Image>
+            <S.ImgAva
+              src="https://placehold.co/800x450"
+              alt="placeholder"
+            ></S.ImgAva>
+          </S.Image>
+          <S.ChangeAva>
+            <S.BtnChangeAva>Change Avatar</S.BtnChangeAva>
+          </S.ChangeAva>
+        </S.Avatar>
+        <S.Gender>
+          <S.Text>Gender</S.Text>
+          <S.GenderType>
+            <input type="checkbox" /> Male
+          </S.GenderType>
+          <S.GenderType>
+            <input type="checkbox" /> Femal
+          </S.GenderType>
+          <S.GenderType>
+            <input type="checkbox" /> Other
+          </S.GenderType>
+        </S.Gender>
+        <S.Birthday>
+          <S.Text>Birthday</S.Text>
+          <S.Date>
+            <input type="date" />
+          </S.Date>
+        </S.Birthday>
+      </S.RightContainer>
+    </S.Container>
+  );
+};
+
+export default MiddleForm;
--- src/routes/defaultLayoutRoutes.tsx
+++ src/routes/defaultLayoutRoutes.tsx
@@ -4,6 +4,7 @@ import { IRoute } from "../interfaces";
 import Home from "../pages/Home/Home";
 import EventPage from "../pages/EventPage/EventPage";
 import AddContributions from "../pages/Contributions/AddContributions/AddContributions";
+import EditProfile from "../pages/EditProfifle/EditProfile";
 
 export const defaultLayoutRoutes: IRoute[] = [
   {
@@ -17,5 +18,9 @@ export const defaultLayoutRoutes: IRoute[] = [
   {
     path: "/addcontributions",
     component: <AddContributions />,
+  },
+  {
+    path: "/editprofile",
+    component: <EditProfile />,
   },
 ];

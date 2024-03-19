71567d643857914a7fa444125bdd1304d4353f26
vclong2003 <vclong2003@gmail.com>
2024-03-19 12:00:19 +0700

    fixes

--- src/components/ManagementComponent/Menu/Menu.tsx
+++ src/components/ManagementComponent/Menu/Menu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AdminMenu, Logo } from "./Menu.styled";
 import { Link } from "react-router-dom";
-import { MdAccountCircle, MdOutlineEventAvailable } from "react-icons/md";
+import { MdOutlineEventAvailable } from "react-icons/md";
 import { LuSchool } from "react-icons/lu";
 import { FaUsers, FaFile, FaHome } from "react-icons/fa";
 export const Menu = () => {
@@ -11,34 +11,29 @@ export const Menu = () => {
         <Logo>
           <h2>ALHKQ</h2>
         </Logo>
-        <Link to="/admin">
+        <Link to="/manage">
           <button>
             <FaHome /> Home
           </button>
         </Link>
 
-        <Link to="/admin/account">
-          <button>
-            <MdAccountCircle /> Your Profile
-          </button>
-        </Link>
-        <Link to="/admin/faculty">
+        <Link to="/manage/faculty">
           <button>
             <LuSchool /> Faculty
           </button>
         </Link>
 
-        <Link to="/admin/user">
+        <Link to="/manage/user">
           <button>
             <FaUsers /> User
           </button>
         </Link>
-        <Link to="/admin/event">
+        <Link to="/manage/event">
           <button>
             <MdOutlineEventAvailable /> Events
           </button>
         </Link>
-        <Link to="/admin/contribution">
+        <Link to="/manage/contribution">
           <button>
             <FaFile /> Contributions
           </button>
--- src/pages/managementPages/Home/Home.tsx
+++ src/pages/managementPages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Headline, Button, ListOfActions } from "./Home.styled";
 import { Link } from "react-router-dom";
-import { MdAccountCircle, MdOutlineEventAvailable } from "react-icons/md";
+import { MdOutlineEventAvailable } from "react-icons/md";
 import { LuSchool } from "react-icons/lu";
 import { FaUsers, FaFile } from "react-icons/fa";
 const Home = () => {
@@ -9,33 +9,27 @@ const Home = () => {
     <>
       <Headline>Dashboard</Headline>
       <ListOfActions>
-        <Link to="/admin/account">
-          <Button color="#DF6A2E">
-            <MdAccountCircle /> Your profile
-          </Button>
-        </Link>
-
-        <Link to="/admin/user">
+        <Link to="/manage/user">
           <Button color="#71984A">
             <FaUsers /> Users
           </Button>
         </Link>
       </ListOfActions>
       <ListOfActions>
-        <Link to="/admin/faculty">
+        <Link to="/manage/faculty">
           <Button color="#C2C6E8">
             <LuSchool /> Faculty
           </Button>
         </Link>
 
-        <Link to="/admin/contribution">
+        <Link to="/manage/contribution">
           <Button color="#236192">
             <FaFile /> Contributions
           </Button>
         </Link>
       </ListOfActions>
       <ListOfActions>
-        <Link to="/admin/event">
+        <Link to="/manage/event">
           <Button color="#F2BA1D">
             <MdOutlineEventAvailable /> Events
           </Button>

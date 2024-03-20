eb9487b7b600f27a2a1d861606b36934b8cee6c5
vclong2003 <vclong2003@gmail.com>
2024-03-20 13:52:48 +0700

    add drop down common component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "./Badge.styled";
+
+interface IBadgeProps {
+  count: number;
+  children: React.ReactNode;
+}
+
+export default function Badge({ count, children }: IBadgeProps): JSX.Element {
+  return (
+    <S.BadgeContainer>
+      <S.Badge $hidden={count === 0}>{count}</S.Badge>
+      <div>{children}</div>
+    </S.BadgeContainer>
+  );
+}

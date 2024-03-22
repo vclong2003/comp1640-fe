1783ae74d80e571dab8d1eee43eaf03ba778878f
vclong2003 <vclong2003@gmail.com>
2024-03-22 10:07:35 +0700

    ok

---
+++ src/variables/user.variables.ts
@@ -0,0 +1,13 @@
+export enum ERole {
+  Guest = "guest",
+  Student = "student",
+  MarketingManager = "mm",
+  MarketingCoordinator = "mc",
+  Admin = "admin",
+}
+
+export enum EGender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}

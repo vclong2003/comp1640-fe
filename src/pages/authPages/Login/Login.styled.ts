bc3c43e8eb12945fade52c1f28df18ff07dea18e
vclong2003 <vclong2003@gmail.com>
2024-03-27 01:01:19 +0700

    Resolve conflict: OK

--- src/pages/authPages/Login/Login.styled.ts
+++ src/pages/authPages/Login/Login.styled.ts
@@ -4,24 +4,20 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   height: 100vh;
-  justify-content: center;
 `;
 
-export const Background = styled.div`
-  flex: 3;
-`;
+export const Background = styled.div``;
 
 export const Image = styled.img`
   width: auto;
   height: 100%;
 `;
 
-export const FormLogin = styled.div`
+export const RightContent = styled.div`
+  margin-top: var(--s-10);
   display: flex;
   flex-direction: column;
   gap: var(--s-5);
-  flex: 9;
-  padding: var(--s-30) 0;
 `;
 
 export const Title = styled.div`

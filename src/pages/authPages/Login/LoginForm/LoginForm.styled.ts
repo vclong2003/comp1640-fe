1bc5f8888cd45d25ecebf1c7fd4a1e081f56c21f
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-03-27 00:44:00 +0700

    asdasd

--- src/pages/authPages/Login/LoginForm/LoginForm.styled.ts
+++ src/pages/authPages/Login/LoginForm/LoginForm.styled.ts
@@ -1,9 +1,11 @@
+import { Formik } from "formik";
 import styled from "styled-components";
+import { FormButton } from "../../../../components/formComponents";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: var(--s-6);
+  gap: var(--s-5);
 `;
 
 export const Title = styled.div`
@@ -14,29 +16,13 @@ export const Description = styled.div`
   font-size: var(--fs-3xl);
 `;
 
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: var(--s-4);
-`;
-
-export const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const Label = styled.label`
-  font-size: var(--fs-2xl);
-`;
-
-export const Input = styled.input`
-  padding: var(--s-4) var(--s-3);
-  font-size: var(--fs-xl);
-  border: 1px solid;
-`;
+export const FormContainer = styled(Formik)``;
 
 export const LinkRegis = styled.div`
-  font-size: var(--fs-2xl);
+  font-size: var(--fs-xl);
+  color: var(--yellow);
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 export const Button = styled.div`
@@ -44,15 +30,6 @@ export const Button = styled.div`
   flex-direction: column;
   gap: var(--s-5);
   padding: 0 var(--s-40);
-`;
-
-export const BtnLogin = styled.button`
-  background-color: #f2ba1d;
-  padding: var(--s-3) 0;
-  font-size: var(--fs-xl);
-  color: white;
-  border-radius: var(--fs-xs);
-  cursor: pointer;
 `;
 
 export const Divider = styled.div`
@@ -70,11 +47,9 @@ export const Line = styled.div`
   width: 100%;
 `;
 
-export const BtnLoginWithGoogle = styled.button`
-  background-color: #898989;
-  padding: var(--s-3) 0;
-  font-size: var(--fs-xl);
-  color: white;
-  border-radius: var(--fs-xs);
-  cursor: pointer;
+export const BtnLoginWithGoogle = styled(FormButton)`
+  margin: 0;
+  background-color: var(--white);
+  color: var(--blue-gray);
+  border: 2px solid var(--blue-gray);
 `;

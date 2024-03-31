c75fb7feffb348a1fd1b2df264bc144f3f3e754a
vclong2003 <vclong2003@gmail.com>
2024-03-31 16:12:10 +0700

    Add text shadows and fix react-big-calendar styling

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -1,3 +1,4 @@
+import * as S from "./Event.styled";
 import moment from "moment";
 import {
   Calendar,
@@ -5,10 +6,15 @@ import {
   momentLocalizer,
 } from "react-big-calendar";
 import Container from "@components/Container/Container";
-
 const localizer = momentLocalizer(moment);
 
-const events: EventItem[] = [
+import PLaceholderBanner from "@assets/images/banner_placeholder.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@store/index";
+import { useEffect } from "react";
+import { findEvents } from "@store/event/eventActions";
+
+const eventsTest: EventItem[] = [
   {
     title: "Test event",
     start: new Date("2024-03-20T10:00:00"),
@@ -30,16 +36,54 @@ const events: EventItem[] = [
 ];
 
 export default function Event() {
+  const { events } = useSelector((state: RootState) => state.eventState);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(findEvents({}));
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log(events);
+  }, [events]);
+
+  const eventStyleGetter = (event: EventItem) => {
+    const backgroundColor =
+      event.resource === "you select event 1" ? "red" : "blue";
+    const style = {
+      backgroundColor,
+    };
+    return {
+      style,
+    };
+  };
+
   return (
     <Container>
-      <Calendar
-        localizer={localizer}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        events={events}
-        onSelectEvent={(event) => alert(event.resource)}
-      />
+      <S.Event>
+        <S.BannerImageContainer>
+          <S.BannerImage src={PLaceholderBanner} alt="Banner" />
+          <S.BannerText>
+            <S.FacultyName>Gryffindor</S.FacultyName>
+            <S.FacultyDescription>
+              Gryffindors are known for being bold, courageous, and adventurous.
+              Brave Gryffindors never shy away from a challenge, and they
+              fearlessly pursue their goals. Gryffindors are chivalrous and have
+              strong moral compasses. They believe in using their strength and
+              courage to do what's right.
+            </S.FacultyDescription>
+          </S.BannerText>
+        </S.BannerImageContainer>
+        <Calendar
+          localizer={localizer}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          events={eventsTest}
+          onSelectEvent={(event) => alert(event.resource)}
+          eventPropGetter={eventStyleGetter}
+        />
+      </S.Event>
     </Container>
   );
 }

67a3efaab50fde7569f0ee69b04bbd5502a0bb9f
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-04-02 10:28:19 +0700

    abou us

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,27 +1,27 @@
-import * as S from './AboutUs.styled';
+import Container from "@components/Container/Container";
+import * as S from "./AboutUs.styled";
+import image from "../../assets/images/unsplash_JaoVGh5aJ3E.png";
 
 const AboutUs = () => {
   return (
-    <S.Container>
-      <S.Header>
-        <S.Title>About Us</S.Title> 
-      </S.Header>
-      <S.Section>
-        <S.ContentContainer>
-          <S.ContentTitle>We Help To Get Solutions</S.ContentTitle>
-          <S.ContentParagraph>
-            Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s.
-            The lightness of a color can be described as how much light you want to give the color, where 0% means no light (black), 50% means
-          </S.ContentParagraph>
-          <S.ButtonContainer>
-            <S.Button>OUR TEAM</S.Button>
-          </S.ButtonContainer>
-        </S.ContentContainer>
-        <S.Image src="https://www.w3schools.com/w3images/team1.jpg" alt="" />
-      </S.Section>
-    </S.Container>
+    <Container>
+      <S.Container>
+        <S.Title>About Us</S.Title>
+
+        <S.LeftContent>
+          <S.Text>We Help To Get Solutions</S.Text>
+          <S.Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </S.Description>
+          <S.Button>Our Team</S.Button>
+        </S.LeftContent>
+        <S.RightContent>
+          <S.Image src={image}></S.Image>
+        </S.RightContent>
+      </S.Container>
+    </Container>
   );
 };
 

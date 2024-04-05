be3d3893675a771b99d00b75c6dca28db315f98b
vclong2003 <vclong2003@gmail.com>
2024-04-05 22:52:29 +0700

    Update imports in DefaultLayout and AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router";
-
+// import ManagementLayout from "src/layouts/ManagementLayout/ManagementLayout";
+import DefaultLayout from "src/layouts/DefaultLayout";
+import ManagementLayout from "src/layouts/ManagementLayout/ManagementLayout";
 import { defaultLayoutRoutes } from "./defaultLayoutRoutes";
 import { authRoutes } from "./authRoutes";
 import { managementRoutes } from "./managementRoutes";
 import LandingPage from "@pages/LandingPage/LandingPage";
 import AuthLayout from "src/layouts/AuthLayout";
-// import ManagementLayout from "src/layouts/ManagementLayout/ManagementLayout";
-import DefaultLayout from "src/layouts/DefaultLayout";
-import ManagementLayout from "src/layouts/ManagementLayout/ManagementLayout";
 
 export default function AppRoutes(): JSX.Element {
   return (

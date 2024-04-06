0ae8c2596d4b5188576359a24da2e6c8e62449c0
Trần Việt Anh <95759256+tranvietanh20112001@users.noreply.github.com>
2024-04-06 09:30:58 +0700

    fix

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,6 @@
 import Footer from "@components/Footer/Footer";
 import Navbar from "@components/Navbar/Navbar";
 import { Outlet } from "react-router";
-<<<<<<< HEAD
-import Navbar from "@components/Navbar/Navbar";
-import Footer from "@components/Footer/Footer";
-=======
->>>>>>> be3d3893675a771b99d00b75c6dca28db315f98b
 
 export default function DefaultLayout(): JSX.Element {
   return (

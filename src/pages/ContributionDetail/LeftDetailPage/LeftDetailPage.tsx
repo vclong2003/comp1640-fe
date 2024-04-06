42edcc99720caeaeb01e4c3a5dafd497ee389789
vclong2003 <vclong2003@gmail.com>
2024-04-06 23:08:51 +0700

    fixes

--- src/pages/ContributionDetail/LeftDetailPage/LeftDetailPage.tsx
+++ src/pages/ContributionDetail/LeftDetailPage/LeftDetailPage.tsx
@@ -2,30 +2,24 @@ import * as S from "./LeftDetailPage.styled";
 import Detail from "../../../assets/images/detail.png";
 import { IoMdPerson } from "react-icons/io";
 import { IoSend } from "react-icons/io5";
-// import { useState } from "react";
-// import ReactQuill from "react-quill";
+
 import Avatar from "@components/Avatar/Avatar";
 import UserInfo from "../UserInfo/UserInfo";
 import { FaHeart } from "react-icons/fa";
 import { IContribution } from "@interfaces/contribution.interfaces";
 
-interface ILeftDetailPageProps{
+interface ILeftDetailPageProps {
   contribution: IContribution;
 }
 
-
-
-const LeftDetailPage = ({contribution}:ILeftDetailPageProps) => {
-
-
-
+const LeftDetailPage = ({ contribution }: ILeftDetailPageProps) => {
   return (
     <S.Container>
       <S.TopContainer>
-        <S.Image src={contribution?.banner_image_url || Detail} />
+        <S.Image src={contribution.banner_image_url || Detail} />
         <S.Text>
           <S.Status>
-            {contribution?.is_publication && (
+            {contribution.is_publication && (
               <S.TextStatus>Published</S.TextStatus>
             )}
           </S.Status>
@@ -40,41 +34,11 @@ const LeftDetailPage = ({contribution}:ILeftDetailPageProps) => {
         </S.Bottom>
       </S.TopContainer>
       <S.MiddleContainer>
-        {/* <div>
-          <ReactQuill
-            value={test}
-            onChange={setTest}
-            placeholder="Test rich text editor"
-            modules={{
-              toolbar: [
-                [{ header: [1, 2, 3, false] }],
-                ["bold", "italic"],
-                [{ list: "ordered" }, { list: "bullet" }],
-                ["link", "image"],
-                ["clean"],
-              ],
-            }}
-          />
-        </div> */}
         <S.ContainerDescription>
-          <S.Description>
-            It is shown by default, until the collapse plugin adds the
-            appropriate classes that we use to style each element. These classes
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the .accordion-body, though the
-            transition does limit overflow. It is shown by default, until the
-            collapse plugin adds the appropriate classes that we use to style
-            each element. These classes control the overall appearance, as well
-            as the showing and hiding via CSS transitions. You can modify any of
-            this with custom CSS or overriding our default variables. It's also
-            worth noting that just about any HTML can go within
-            the .accordion-body, though the transition does limit overflow.
-          </S.Description>
+          <S.Description>{contribution.description}</S.Description>
           <S.ContainerLike>
             <FaHeart />
-            <S.Like>100</S.Like>
+            <S.Like>{contribution.likes}</S.Like>
           </S.ContainerLike>
         </S.ContainerDescription>
       </S.MiddleContainer>

51e99dad268b404d5394ae812321a11077299407
vclong2003 <vclong2003@gmail.com>
2024-04-08 14:13:05 +0700

    okok

--- src/interfaces/user.interfaces.ts
+++ src/interfaces/user.interfaces.ts
@@ -105,6 +105,7 @@ export interface IUpdateUserPayload {
   dob?: string;
   gender?: EGender;
   avatar?: File;
+  facultyId?: string;
 }
 // Find users --------------------------------
 export interface IFindUsersPayload {

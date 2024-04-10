422dcc712f91add1be474a9a114ac0289bb1cda4
vclong2003 <vclong2003@gmail.com>
2024-04-10 15:30:24 +0700

    Update responsive breakpoints and fix home page contribution display

--- src/config/responsiveBreakpoints.ts
+++ src/config/responsiveBreakpoints.ts
@@ -1,15 +1,15 @@
-const SIZES = {
-  PHONE: "576px",
-  PHONE_L: "768px",
-  TABLET: "992px",
-  DESKTOP: "1200px",
-  DESKTOP_L: "1400px",
+export const SIZES = {
+  PHONE: "576p",
+  PHONE_L: "768",
+  TABLET: "992",
+  DESKTOP: "1200",
+  DESKTOP_L: "1400",
 };
 
 export const DEVICES = {
-  PHONE: `(min-width: ${SIZES.PHONE})`,
-  PHONE_L: `(min-width: ${SIZES.PHONE_L})`,
-  TABLET: `(min-width: ${SIZES.TABLET})`,
-  DESKTOP: `(min-width: ${SIZES.DESKTOP})`,
-  DESKTOP_L: `(min-width: ${SIZES.DESKTOP_L})`,
+  PHONE: `(min-width: ${SIZES.PHONE}px)`,
+  PHONE_L: `(min-width: ${SIZES.PHONE_L}px)`,
+  TABLET: `(min-width: ${SIZES.TABLET}px)`,
+  DESKTOP: `(min-width: ${SIZES.DESKTOP}px)`,
+  DESKTOP_L: `(min-width: ${SIZES.DESKTOP_L}px)`,
 };

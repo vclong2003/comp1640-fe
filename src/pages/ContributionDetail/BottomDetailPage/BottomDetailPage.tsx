b7e120cfba7852d422055d272ff7431983b0fc10
anhquan2610 <Quanddagch200326@fpt.edu.vn>
2024-04-17 10:56:32 +0700

    Refactor BottomDetailPage component to include delete functionality

--- src/pages/ContributionDetail/BottomDetailPage/BottomDetailPage.tsx
+++ src/pages/ContributionDetail/BottomDetailPage/BottomDetailPage.tsx
@@ -4,15 +4,16 @@ import { RootState } from "@store/index";
 import { ERole } from "@interfaces/user.interfaces";
 import { IContribution } from "@interfaces/contribution.interfaces";
 
-
 interface IBottomDetailPageProps {
   contribution: IContribution;
   onPublish: () => void;
+  onDelete: () => void;
 }
 
 export default function BottomDetailPage({
   contribution,
   onPublish,
+  onDelete,
 }: IBottomDetailPageProps) {
   const { user } = useSelector((state: RootState) => state.userState);
 
@@ -22,13 +23,23 @@ export default function BottomDetailPage({
       onPublish();
     }
   };
+  const handleDelete = () => {
+    const shouldDelete = window.confirm("Are you sure you want to delete?");
+    if (shouldDelete) {
+      onDelete();
+    }
+  };
 
   return (
-    user?.role === ERole.MarketingCoordinator &&
-    !contribution.is_publication && (
-      <S.Container>
-        <S.BtnPublish onClick={handlePublish}>Publish</S.BtnPublish>
-      </S.Container>
-    )
+    <S.Container>
+      {user?.role === ERole.MarketingCoordinator &&
+        !contribution.is_publication && (
+          <S.BtnPublish onClick={handlePublish}>Publish</S.BtnPublish>
+        )}
+      {(user?.role === ERole.MarketingCoordinator ||
+        user?._id === contribution.author._id) && (
+        <S.BtnPublish onClick={handleDelete}>Delete</S.BtnPublish>
+      )}
+    </S.Container>
   );
 }
